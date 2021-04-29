74ba287e9120a46b62654f5938d5b8a63f0c9005
Vivek Kalyan <hello@vivekkalyan.com>
2021-04-30 01:42:49 +0800

    Change bert to multilingual bert and test on es,pr

--- subtask4/src/models/multilingual_bert.py
+++ subtask4/src/models/multilingual_bert.py
@@ -89,6 +89,8 @@ class NERDataset(torch.utils.data.Dataset):
         return len(self.labels)
 
 texts, tags = read_data('data/en-orig.txt')
+es_texts, es_tags = read_data('data/es-orig.txt')
+pr_texts, pr_tags = read_data('data/pr-orig.txt')
 
 train_texts, val_texts, train_tags, val_tags = train_test_split(texts, tags, test_size=.2)
 
@@ -96,22 +98,30 @@ unique_tags = set(tag for doc in tags for tag in doc)
 tag2id = {tag: id for id, tag in enumerate(unique_tags)}
 id2tag = {id: tag for tag, id in tag2id.items()}
 
-tokenizer = BertTokenizerFast.from_pretrained('bert-base-cased')
+tokenizer = BertTokenizerFast.from_pretrained('bert-base-multilingual-cased')
 train_encodings = tokenizer(train_texts, is_split_into_words=True, padding=True, truncation=True)
 val_encodings = tokenizer(val_texts, is_split_into_words=True, padding=True, truncation=True)
+es_encodings = tokenizer(es_texts, is_split_into_words=True, padding=True, truncation=True)
+pr_encodings = tokenizer(pr_texts, is_split_into_words=True, padding=True, truncation=True)
 
 train_labels = align_labels(train_tags, train_encodings)
 val_labels = align_labels(val_tags, val_encodings)
+es_labels = align_labels(es_tags, es_encodings)
+pr_labels = align_labels(pr_tags, pr_encodings)
 
 train_dataset = NERDataset(train_encodings, train_labels)
 val_dataset = NERDataset(val_encodings, val_labels)
+es_dataset = NERDataset(es_encodings, es_labels)
+pr_dataset = NERDataset(pr_encodings, pr_labels)
 
-train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=4, shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=2, shuffle=True)
+es_loader = DataLoader(es_dataset, batch_size=2, shuffle=True)
+pr_loader = DataLoader(pr_dataset, batch_size=2, shuffle=True)
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-model = BertForTokenClassification.from_pretrained('bert-base-cased', num_labels=len(unique_tags))
+model = BertForTokenClassification.from_pretrained('bert-base-multilingual-cased', num_labels=len(unique_tags))
 optim = AdamW(model.parameters(), lr=5e-5)
 
 model.to(device)
@@ -138,10 +148,10 @@ for epoch in range(5):
         labels = batch['labels'].to(device)
         outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
         loss = outputs[0]
-        logits = outputs[1]
+        logits = outputs[1].to('cpu').detach()
         preds = torch.argmax(logits, 2)
         val_pred = preds[attention_mask > 0]
-        val_label = labels[attention_mask > 0]
+        val_label = batch['labels'][attention_mask > 0]
         val_pred = val_pred[val_label > -1]
         val_label = val_label[val_label > -1]
         val_preds.append(val_pred)
@@ -151,7 +161,53 @@ for epoch in range(5):
     val_labels = sum(map(lambda x: x.tolist(), val_labels), [])
     val_preds_tag = [id2tag[i] for i in val_preds]
     val_labels_tag = [id2tag[i] for i in val_labels]
-    conll_evaluate(val_labels_tag, val_preds_tag)
+    _, _, f1 = conll_evaluate(val_labels_tag, val_preds_tag)
+
+    es_preds = []
+    es_labels = []
+    for batch in tqdm(es_loader):
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        logits = outputs[1].to('cpu').detach()
+        preds = torch.argmax(logits, 2)
+        es_pred = preds[attention_mask > 0]
+        es_label = batch['labels'][attention_mask > 0]
+        es_pred = es_pred[es_label > -1]
+        es_label = es_label[es_label > -1]
+        es_preds.append(es_pred)
+        es_labels.append(es_label)
+
+    es_preds = sum(map(lambda x: x.tolist(), es_preds), [])
+    es_labels = sum(map(lambda x: x.tolist(), es_labels), [])
+    es_preds_tag = [id2tag[i] for i in es_preds]
+    es_labels_tag = [id2tag[i] for i in es_labels]
+    _, _, es_f1 = conll_evaluate(es_labels_tag, es_preds_tag)
+
+    pr_preds = []
+    pr_labels = []
+    for batch in tqdm(pr_loader):
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        logits = outputs[1].to('cpu').detach()
+        preds = torch.argmax(logits, 2)
+        pr_pred = preds[attention_mask > 0]
+        pr_label = batch['labels'][attention_mask > 0]
+        pr_pred = pr_pred[pr_label > -1]
+        pr_label = pr_label[pr_label > -1]
+        pr_preds.append(pr_pred)
+        pr_labels.append(pr_label)
+
+    pr_preds = sum(map(lambda x: x.tolist(), pr_preds), [])
+    pr_labels = sum(map(lambda x: x.tolist(), pr_labels), [])
+    pr_preds_tag = [id2tag[i] for i in pr_preds]
+    pr_labels_tag = [id2tag[i] for i in pr_labels]
+    _, _, pr_f1 = conll_evaluate(pr_labels_tag, pr_preds_tag)
+
+    print(f"f1 en: {f1}, es: {es_f1}, pr: {pr_f1}")
 
 model.eval()
 torch.save(model.state_dict(), 'baseline_bert.model')
